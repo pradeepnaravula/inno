52b0cd8af2844d4ea6ada17f132d284e8e0e4604
pradeepnaravula <116377952+pradeepnaravula@users.noreply.github.com>
2024-01-08 22:47:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Welcome to NOIDA" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Welcome" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, It works!" }');
+});
+app.listen(process.env.PORT || 3000);
+module.exports = app;
